1b66d208b5837f5055e42090eb77f1260f0837f2
Ben Geier <bengeierl97@gmail.com>
2019-12-23 12:04:37 -0500

    Parsed and filtered song

--- convert.py
+++ convert.py
@@ -1,14 +1,20 @@
 from PIL import Image, ImageDraw
 
 if __name__ == '__main__':
-	height = 600
-	width = 600 
-	image = Image.new(mode='RGB', size=(height,width), color=(0,0,0))
+	# height = 600
+	# width = 600 
+	# image = Image.new(mode='RGB', size=(height,width), color=(0,0,0))
 
-	size = 10
+	# size = 10
 
-	for y in range(0, height, size):
-		for x in range(0, width, size):
-			ImageDraw.Draw(image).rectangle([x,y,x+10,y+10],(x,y,x),(0,0,0),1)
+	# for y in range(0, height, size):
+	# 	for x in range(0, width, size):
+	# 		ImageDraw.Draw(image).rectangle(
+	# 			[x,y,x+10,y+10],(x,y,x),(0,0,0),1)
 
-	image.show()
+	# image.show()
+
+	song = open("powerful.txt", "r").read().split()
+	for word in range(len(song)):
+		song[word] = song[word].lower().strip(",")
+	print(song)
