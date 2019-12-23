da80a44c18508a07f4e6f686f6eb93d38f72fc43
Ben Geier <bengeierl97@gmail.com>
2019-12-23 11:29:14 -0500

    Creates pretty colorful boxes

--- convert.py
+++ convert.py
@@ -1,8 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 if __name__ == '__main__':
 	height = 600
 	width = 600 
-	image = Image.new(mode='L', size=(height,width), color=255)
+	image = Image.new(mode='RGB', size=(height,width), color=(0,0,0))
+
+	size = 10
+
+	for y in range(0, height, size):
+		for x in range(0, width, size):
+			ImageDraw.Draw(image).rectangle([x,y,x+10,y+10],(x,y,x),(0,0,0),1)
 
 	image.show()
