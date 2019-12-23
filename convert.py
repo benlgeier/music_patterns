be6036bd113de2301d156e0a225fdcd4f313780c
Ben Geier <bengeierl97@gmail.com>
2019-12-23 12:26:03 -0500

    Create grid with matching words

--- convert.py
+++ convert.py
@@ -1,20 +1,31 @@
 from PIL import Image, ImageDraw
 
 if __name__ == '__main__':
-	# height = 600
-	# width = 600 
-	# image = Image.new(mode='RGB', size=(height,width), color=(0,0,0))
 
-	# size = 10
+	song = open("powerful.txt", "r").read().split()
+	for word in range(len(song)):
+		song[word] = song[word].lower().strip(",")
+	
+	box_size = 10
+	song_len = len(song)
+	img_size = song_len * box_size
 
-	# for y in range(0, height, size):
-	# 	for x in range(0, width, size):
+	song_grid = [["" for i in range(song_len)] for j in range(song_len)]
+	
+	for y in range(song_len):
+		for x in range(song_len):
+			if song[y] == song[x]:
+				song_grid[y][x] = song[x]
+
+	print(song_grid)
+
+
+	# image = Image.new(mode='RGB', size=(img_size, img_size), color=(0,0,0))
+
+	# for y in range(0, img_size, box_size):
+	# 	for x in range(0, img_size, box_size):
 	# 		ImageDraw.Draw(image).rectangle(
 	# 			[x,y,x+10,y+10],(x,y,x),(0,0,0),1)
 
 	# image.show()
 
-	song = open("powerful.txt", "r").read().split()
-	for word in range(len(song)):
-		song[word] = song[word].lower().strip(",")
-	print(song)
